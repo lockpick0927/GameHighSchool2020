4db7465a7a5783a2940e09ee0f1808573d36f52f
lockpick0927 <lockpick0927@naver.com>
2020-07-01 19:25:13 +0900

    탄알 피하기 게임 최초 푸쉬

---
+++ 06-26/Assets/Dodge/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float M_Speed = 20.0f;
+    public int Times = 150; //초당 30
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidbody = /*gameObject.*/GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        rigidbody.AddForce(new UnityEngine.Vector3(xAxis, 0, yAxis) * M_Speed);
+
+        float FireAxis = Input.GetAxis("Fire1");
+        
+        Debug.Log(Times);
+        Times--;
+        if (FireAxis > Times)
+        {
+            Die();
+        }
+        /*
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(Vector3.left * M_Speed);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(Vector3.right * M_Speed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(Vector3.forward * M_Speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(Vector3.back * M_Speed);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Die();
+        }
+        */
+        
+    }
+
+    public void Die()
+    {
+        Debug.Log("사망");
+        gameObject.SetActive(false);
+    }
+}
---
+++ 06-26/Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Test : EditorWindow
+{
+    [MenuItem("Window/Test")]
+
+
+
+    static void Init()
+    {
+        Test window = (Test)EditorWindow.GetWindow(typeof(Test));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        Handles.color = Color.red;
+        Handles.DrawRectangle(0, new Vector3(200, 200), Quaternion.identity, 100);
+        Handles.color = Color.black;
+        Handles.DrawSolidDisc(new Vector3(150, 100, 0), Vector3.forward, 50);
+        Handles.DrawSolidDisc(new Vector3(250, 100, 0), Vector3.forward, 50);
+        Handles.color = Color.white;
+        Handles.DrawSolidDisc(new Vector3(200, 200, 0), Vector3.forward, 100);
+    }
+}
