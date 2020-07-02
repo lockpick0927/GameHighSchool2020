f5d495db0c28c373ce8f8ca32a5dd54c02019b9c
lockpick0927 <lockpick0927@naver.com>
2020-07-02 21:18:37 +0900

    3일차 총알 피하기 게임

---
+++ 06-26/Assets/Dodge/Scripts/BulletSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform m_PlayerTransfrom;
+
+    public GameObject m_Bullet;
+
+    public float Tick = 0f;
+    public float m_RotationSpeed = 60f;
+
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+
+            Tick += Time.deltaTime;
+            if (Tick > 1)
+            {
+                GameObject bullet = GameObject.Instantiate(m_Bullet);
+                bullet.transform.position = transform.position;
+                bullet.transform.rotation = transform.rotation;
+
+                Tick = 0;
+            }
+
+            
+            Vector3 attackPoint = player.transform.position;
+            attackPoint.y = transform.position.y;
+            transform.LookAt(attackPoint);
+        }
+    }
+
+}
+//GameObject.Find("게임오브젝트의 이름");
+//GameObject player = GameObject.FindGameObjectWithTag("Player");
+// GameObject.FindObjectOfType<PlayerController>();
+
+//GameObject.FindGameObjectsWithTag("모든 태그");
+//GameObject.FindSceneObjectsOfType<PlayerController>(); 모든 PlayerController 검색
