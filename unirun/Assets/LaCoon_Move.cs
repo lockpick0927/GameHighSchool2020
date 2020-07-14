9f5bb7bbb8d4493e61c0b8e676b47850eccaa632
lockpick0927 <lockpick0927@naver.com>
2020-07-14 21:26:30 +0900

    유니런 2일차

---
+++ unirun/Assets/LaCoon_Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaCoon_Move : MonoBehaviour
+{
+    public float m_Speed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        //주석 : 설명 필요없는 스크립트를 임시적으로 비활성화하기 위해서 사용
+        /* 주석 */
+        Rigidbody2D rigidbody = /*gameObject.*/GetComponent<Rigidbody2D>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(new Vector2(xAxis, yAxis) * m_Speed);
+
+
+        //정답
+        //float fireAxis = Input.GetAxis("Fire1");
+
+        //if (fireAxis > 0.95f)
+        //    Die();
+    }
+
+    //이게 첫줄
+    //public GameManager m_GameManager;
+
+    public void Die()
+    {
+        Debug.Log("사망");
+        //gameObject.SetActive(false)
+    }
+}
---
+++ unirun/Assets/Script/Player_Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public Animator m_Animator;
+
+    public bool m_IsGround = false;
+    public bool m_Dead = false;
+    public float m_Speed = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Animator.SetBool("IsGround", m_IsGround);
+
+        Rigidbody2D rigidbody = /*gameObject.*/GetComponent<Rigidbody2D>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        float J_Axis = Input.GetAxis("Jump");
+
+        rigidbody.AddForce(new Vector2(xAxis, yAxis) * m_Speed);
+        if(m_IsGround == true) rigidbody.AddForce(new Vector2(0, J_Axis) * 300f);
+        //m_Animator.SetBool("IsDead", m_Dead);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            m_IsGround = true;
+        }        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            m_IsGround = false;
+        }
+    }
+
+
+}
